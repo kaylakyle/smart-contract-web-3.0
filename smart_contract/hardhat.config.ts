19b6395f754033e4136658ad3dfe11e2757d5e63
lynnkati <131492945+kaylakyle@users.noreply.github.com>
2026-03-23 15:49:18 +0300

    Update hardhat.config.ts

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,19 +1,12 @@
-import { ethers } from "ethers";
+import { defineConfig } from "hardhat/config";
 
-async function main() {
-  // Connect to local Hardhat node
-  const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
-
-  // Get accounts (signers)
-  const signers = await provider.listAccounts();
-
-  // Print each account address
-  signers.forEach((account, i) => {
-    console.log(`Account #${i}: ${account}`);
-  });
-}
-
-main().catch((error) => {
-  console.error(error);
-  process.exit(1);
+export default defineConfig({
+  solidity: {
+    version: "0.8.28",
+  },
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+  },
 });

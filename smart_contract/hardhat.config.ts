469a357fdbcdd17cf172574004ad7ecd296dd388
lynnkati <131492945+kaylakyle@users.noreply.github.com>
2026-03-23 13:40:47 +0300

    Update hardhat.config.ts

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox"; // <- add this
 
 export default defineConfig({
   solidity: {
     version: "0.8.28",
+  },
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
   },
 });
